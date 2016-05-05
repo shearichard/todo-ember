c4ca9593eee981263dfc461d118bda8a0955ab21
Richard Shea <rshea@thecubagroup.com>
2016-05-05 19:18:11 +1200

    Change each of the 'todos' nested routes so that if a user attempts to access them without having logged on they will be kicked back to the login screen

--- app/routes/todos/new.js
+++ app/routes/todos/new.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import SaveModelMixin from 'todo-ember/mixins/todos/save-model-mixin';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(SaveModelMixin, {
+export default Ember.Route.extend(SaveModelMixin, AuthenticatedRouteMixin, {
   model: function() {
     return this.store.createRecord('todo');
   }
