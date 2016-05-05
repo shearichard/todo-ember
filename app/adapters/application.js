db033ce111e7cfa69222ec061aec4b561a29ad32
Richard Shea <rshea@thecubagroup.com>
2016-05-05 19:02:43 +1200

    Change to the adapter application.js to add the 'authorizer' header and the relevant value to every request to the backend

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,9 @@
 import DS from 'ember-data';
 import ENV from 'todo-ember/config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend({
-    host: ENV.host,
-    namespace: 'api'
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+  host: ENV.host,
+  namespace: 'api',
+  authorizer: 'authorizer:drf-token-authorizer'
 });
